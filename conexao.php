329ced73ab4ce1901a256e63f301a891f587f15c
Alefesb <142458915+Alefesb@users.noreply.github.com>
2024-12-12 20:31:55 -0300

    att

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $pdo = new PDO ("mysql:dbname=academia;host=localhost:3306","root","pokemon01");
+    $pdo = new PDO ("mysql:dbname=academia;host=localhost:3306","root","");
     //if($pdo){
        // echo "Funcionando";
     //}else{
